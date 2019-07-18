3b086e3ee973494dae868e6858576f2fa63a32f4
stib <stib@pureandapplied.com.au>
2019-07-18 16:35:16 +1000

    added a note about the console in the description

--- CountDownTimer.psm1
+++ CountDownTimer.psm1
@@ -4,10 +4,12 @@
       <#
 
     .SYNOPSIS
-    Displays a text-based countdown timer
+    Displays a text-based countdown timer in the console.
 
     .DESCRIPTION
-    Displays a timer counting down the seconds until the script terminates. Parameters control the length that it runs for.
+    Displays a timer counting down the seconds until the script terminates. 
+    Parameters control the length that it runs for.
+    Only works in the console, because of some console tricks used to display the output.
 
     .EXAMPLE
     ./Start-CountdownTimer -Hours 1 -Minutes 2 -Seconds 3
@@ -38,19 +40,19 @@
       [int]$Seconds = 0,
       [int]$TickLength = 1
     )
-    $t = New-TimeSpan -Days $Days -Hours $Hours -Minutes $Minutes -Seconds $Seconds;
-    $origpos = $host.UI.RawUI.CursorPosition;
-    $spinner =@('|', '/', '-', '\');
-    $spinnerPos = 0;
-    $remain = $t;
-    $d =( get-date) + $t;
+    $t = New-TimeSpan -Days $Days -Hours $Hours -Minutes $Minutes -Seconds $Seconds
+    $origpos = $host.UI.RawUI.CursorPosition
+    $spinner =@('|', '/', '-', '\')
+    $spinnerPos = 0
+    $remain = $t
+    $d =( get-date) + $t
     $remain = ($d - (get-date))
     while ($remain.TotalSeconds -gt 0){
       Write-Host (" {0} " -f $spinner[$spinnerPos%4]) -BackgroundColor White -ForegroundColor Black -NoNewline
       write-host (" {0}D {1:d2}h {2:d2}m {3:d2}s    " -f $remain.Days, $remain.Hours, $remain.Minutes, $remain.Seconds)
       $host.UI.RawUI.CursorPosition = $origpos
-      $spinnerPos += 1;
-      Start-Sleep -seconds $TickLength;
+      $spinnerPos += 1
+      Start-Sleep -seconds $TickLength
       $remain = ($d - (get-date))
     }
     $host.UI.RawUI.CursorPosition = $origpos
